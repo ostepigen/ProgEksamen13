06159b9ee280811e326d171f68f046568d1256c5
Anders  Brandt <andersbrandt@Anderss-MacBook-Pro.local>
2024-04-23 13:04:03 +0200

    anders

---
+++ public/login.js
@@ -0,0 +1,50 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+console.log("hej")
+    fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: username, password: password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.success) {
+            window.location.href = 'profile.html'; // Redirect to the profile page
+        } else {
+            alert('Fejl ved login: ' + data.message);
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+
+// lav bruger
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('http://localhost:3001/create-user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: username, password: password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.success) {
+            alert('Bruger oprettet! Log venligst ind.');
+            // Optionally clear the form fields
+            document.getElementById('username').value = '';
+            document.getElementById('password').value = '';
+        } else {
+            alert('Error: ' + data.message);
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});
